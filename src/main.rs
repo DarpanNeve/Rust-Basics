43c6e006570154d985e63b9ba9023acbb95775e9
Darpan Neve <darpanneve3@gmail.com>
2024-08-04 00:06:36 +0530

    basic conditions added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod fibonaacci;
+
 fn main() {
-    println!("Hello, world!");
+    let ans = is_even(9);
+    println!("{}", ans);
+}
+
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        true
+    } else {
+        false
+    }
 }
