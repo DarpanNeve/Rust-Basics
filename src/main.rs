4dc14816f48525b14c807f1bcb711d5a0071e938
Darpan Neve <darpanneve3@gmail.com>
2024-09-24 14:18:14 +0530

    basic fibonacci added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod fibonaacci;
-
+use fibonaacci::fib;
 fn main() {
     let ans = is_even(9);
     println!("{}", ans);
+    println!("The fibonacci series value is {}", fib(10))
 }
 
 fn is_even(num: i32) -> bool {
